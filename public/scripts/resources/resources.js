5b9d7cf0641df56bc060639fcf05053a66b20f40
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-05-23 09:27:14 +0530

    image files added

---
+++ public/scripts/resources/resources.js
@@ -0,0 +1,184 @@
+function load_resources() {
+    document.getElementById("second_box_rigth").innerHTML = `` 
+  let second_box_rigth =  document.getElementById("second_box_rigth") 
+     second_box_rigth.innerHTML = load_data() 
+     function load_data() {
+         return `<h2>All Resources</h2>
+         <div class="line"></div>
+         <div class="popular_guides">
+             <h2>Popular guides</h2>
+             <div class="jss3">
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/8-motorcycle-riding-tips-for-female-bike-riders.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Get started with Two Wheeler Insurence</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/reimbursement-in-health-insurance.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Get started with Health Insurence</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/individual-medical-insurance.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Get started with Group Health Insurence</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+             </div>
+             <div class="jss4">
+                 <a href="#">
+                     <div class="jss41">
+                         Explore more guides
+                         <div class="jss3121">
+                             <img src="../../Images/resources/arrow right.png" alt="">
+                         </div>
+                     </div>
+                 </a>
+             </div>
+         </div>
+         <div class="popular_articles">
+             <h2>Popular articles</h2>
+             <p class="paragraph1">Read articles on insurance to understand insurance related features, benefits and tips in detail</p>
+             <div class="jss3">
+                 <a href="#">
+                     <div class="jss3111">
+                         <div class="jss311">
+                             <img src="../../Images/resources/tips-to-reduce-your-two-wheeler-insurance-premium.webp" alt="">
+                         </div>
+                             <p class="paragraph3">Essential Tips to Reduce Your Two-wheeler Insurance Premium</p>
+                         <div class="jss313">
+                             <p class="paragraph2">Team Acko</p>
+                             <div class="dot"></div>
+                             <p class="paragraph2">May 16, 2022</p>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss3111">
+                         <div class="jss311">
+                             <img src="../../Images/resources/things-to-know-about-car-insurance-for-beginners.webp" alt="">
+                         </div>
+                             <p class="paragraph3">5 Things to Know About Car Insurance for Beginners</p>
+                         <div class="jss313">
+                             <p class="paragraph2">Team Acko</p>
+                             <div class="dot"></div>
+                             <p class="paragraph2">May 16, 2022</p>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss3111">
+                         <div class="jss311">
+                             <img src="../../Images/resources/room-rent-capping-in-health-insurance.webp" alt="">
+                         </div>
+                             <p class="paragraph3">Room Rent Capping Under Health Insurance Coverage (Room Rent Limit)</p>
+                         <div class="jss313">
+                             <p class="paragraph2">Team Acko</p>
+                             <div class="dot"></div>
+                             <p class="paragraph2">May 16, 2022</p>
+                         </div>
+                     </div>
+                 </a>
+             </div>
+             <div class="jss4">
+                 <a href="#">
+                     <div class="jss41">
+                         Explore more articles
+                         <div class="jss3121">
+                             <img src="../../Images/resources/arrow right.png" alt="">
+                         </div>
+                     </div>
+                 </a>
+             </div>
+         </div>
+         <div class="popular_ebooks">
+             <h2>Popular ebooks</h2>
+             <p class="paragraph1">Checkout handy ebooks on insurence</p>
+             <div class="jss3">
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/complete-guide-to-vehicle-insurance.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Complete Guide to Car Insurence Policy [E-Book]</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/car_insurance_add-ons_guides_thumbnail.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Car Insurence Add-ons [E-Book]</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+                 <a href="#">
+                     <div class="jss31">
+                         <div class="jss311">
+                             <img src="../../Images/resources/creating-good-workplace-culture-e-book.webp" alt="">
+                         </div>
+                         <div class="jss312">
+                             <p>Creating a Rewarding Workplace Culture [E-Book]</p>
+                             <div class="jss3121">
+                                 <img src="../../Images/resources/arrow right.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                 </a>
+             </div>
+             <div class="jss4">
+                 <a href="#">
+                     <div class="jss41">
+                         Explore more ebooks
+                         <div class="jss3121">
+                             <img src="../../Images/resources/arrow right.png" alt="">
+                         </div>
+                     </div>
+                 </a>
+             </div>
+         </div>
+         </div>`
+     } 
+
+}
+function load_guides() {
+   document.getElementById("second_box_rigth").innerHTML = ``
+}
+function load_articles() {
+   document.getElementById("second_box_rigth").innerHTML = ``
+}
+function load_ebooks() {
+   document.getElementById("second_box_rigth").innerHTML = ``
+}
