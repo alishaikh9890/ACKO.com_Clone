5f92ab41f80104020f396ee44ce0d9fdd3ee530e
Yashasvi Vij <yashasvivij01@gmail.com>
2022-05-24 12:18:50 +0530

    made navbar as a component and changed the name of homepage file

---
+++ navbarComponent/navbar.js
@@ -0,0 +1,21 @@
+function navbar(){
+    return `
+    <div id = "navBar">
+    <img src = "acko_logo.jfif" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <span>Products</span>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <span>Resources</span>
+    <div class = "floatsRight">
+    <div class = "logIn">
+    <i class="fa fa-file-text" style="color:white"></i>
+    <span>Claim, edit, renew & more</span>
+    <button>Login &nbsp;<i class="arrow down"></i></button>
+    </div>
+    <span class = "help">Help</span>
+    </div>
+</div>
+     `
+}
+
+export default navbar;
